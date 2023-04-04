938638f99c29cf2d33287eeb38294fc6f9d1d5ea
wangdong <wangdong1328@163.com>
2023-04-04 20:35:09 +0800

    上传代码框架

---
+++ cpputil/threadpool.h
@@ -0,0 +1,21 @@
+/**
+ * @file threadpool.h
+ * @author wangdong (wangdong1328@163.com)
+ * @brief 线程池
+ * @version 0.1
+ * @date 2023-04-04
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#ifndef __THREADPOOL_H__
+#define __THREADPOOL_H__
+
+
+class CThreadPool
+{
+    public:
+};
+
+#endif  /* __THREADPOOL_H__ */
---
+++ unittest/threadpool_test.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file threadpool_test.h
+ * @author wangdong (wangdong1328@163.com)
+ * @brief 测试线程池函数
+ * @version 0.1
+ * @date 2023-04-04
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <iostream>
+#include "threadpool.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    std::cout << "hello world" << std::endl;
+    return 0;
+}
